9112dd48d347ada518ef1242db10b2f50c74769a
LpSion <104082083+LpSion@users.noreply.github.com>
2022-10-03 14:29:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let navbar = document.querySelector('.header .flex .navbar');
+let profile = document.querySelector('.header .flex .profile');
+
+function test(){
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
+}
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   navbar.classList.remove('active');
+}
+
+function loader(){
+   document.querySelector('.loader').style.display = 'none';
+}
+
+function fadeOut(){
+   setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
